c4219fd8dc5e2253fa1ec87f5ad1a5480a0aee39
chyroc <chyroc@qq.com>
2022-10-20 13:20:12 +0800

    init

---
+++ main.go
@@ -0,0 +1,125 @@
+package main
+
+import (
+	"fmt"
+	"github.com/urfave/cli/v2"
+	"log"
+	"os"
+	"regexp"
+	"sort"
+	"strings"
+)
+
+func action(c *cli.Context) error {
+	content, err := readHistory()
+	if err != nil {
+		return err
+	}
+	historyList, err := splitHistory(content)
+	if err != nil {
+		return err
+	}
+	sort.Slice(historyList, func(i, j int) bool {
+		return historyList[i].Ts < historyList[j].Ts
+	})
+	historyList = filterHistory(historyList)
+
+	return writeHistory(historyList)
+}
+
+func readHistory() (string, error) {
+	home, err := os.UserHomeDir()
+	if err != nil {
+		return "", err
+	}
+	bs, err := os.ReadFile(home + "/.zsh_history")
+	if err != nil {
+		return "", err
+	}
+	return string(bs), nil
+}
+
+func writeHistory(historyList []*History) error {
+	home, err := os.UserHomeDir()
+	if err != nil {
+		return err
+	}
+	builder := strings.Builder{}
+	for _, v := range historyList {
+		builder.WriteString(fmt.Sprintf(": %s:%s;%s\n", v.Ts, v.Typ, v.Cmd))
+	}
+	return os.WriteFile(home+"/.zsh_history", []byte(builder.String()), 0644)
+}
+
+type History struct {
+	Ts  string
+	Typ string
+	Cmd string
+}
+
+func splitHistory(content string) ([]*History, error) {
+	res := make([]*History, 0)
+	idx := 0
+	list := strings.Split(content, "\n")
+	for idx < len(list) {
+		line := list[idx]
+		for idx+1 < len(list) && list[idx+1] != "" && list[idx+1][0] != ':' {
+			line += list[idx+1]
+			idx++
+		}
+		idx++
+		match := re.FindStringSubmatch(line)
+		if len(match) == 0 {
+			continue
+		}
+		if len(match) != 4 {
+			return nil, fmt.Errorf("invalid history line: %d, %s", len(match), line)
+		} else {
+			res = append(res, &History{
+				Ts:  match[1],
+				Typ: match[2],
+				Cmd: match[3],
+			})
+		}
+	}
+	return res, nil
+}
+
+// 留下第一个，最后一个
+func filterHistory(historyList []*History) []*History {
+	tmp := map[string][]*History{}
+	for _, v := range historyList {
+		tmp[v.Cmd] = append(tmp[v.Cmd], v)
+	}
+
+	res := make([]*History, 0)
+	// 留下第一个，最后一个
+	for _, v := range tmp {
+		if len(v) == 1 {
+			res = append(res, v[0])
+		} else {
+			res = append(res, v[0], v[len(v)-1])
+		}
+	}
+
+	// 排序
+	sort.Slice(res, func(i, j int) bool {
+		return res[i].Ts < res[j].Ts
+	})
+	return res
+
+}
+
+var re = regexp.MustCompile(`: (\d+):(\d+);(.*)`)
+
+func main() {
+	app := &cli.App{
+		Name:   "shortzshistory",
+		Action: action,
+	}
+	err := app.Run(os.Args)
+	if err != nil {
+		panic(err)
+		log.Fatalln(err)
+	}
+}
